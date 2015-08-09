cac766590a6786625c7d859165d4a6f5f92813f1
Sergey Khabaroff <khabaroff@gmail.com>
2015-08-09 15:29:41 +0300

    tuning

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
-    'cache' => 'compilation_cache',
+//    'cache' => 'compilation_cache',
     'auto_reload' => true
 ));
 
