aaef035293f5cd434e3f189897fab0ee7d7d89e8
khabaroff <khabaroff@gmail.com>
2015-08-11 15:39:07 +0300

    gulp 3

--- builder/gulpfile.js
+++ builder/gulpfile.js
@@ -5,24 +5,29 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     rigger = require('gulp-rigger'),
+//   uglify = require('gulp-uglify'),
     browserSync = require("browser-sync"),
     reload = browserSync.reload;
 
 var path = {
     build: {
-        html: '../'
+        html: '../',
+        js: '../static/js/'
     },
     src: {
-        html: '../templates/index.html'
+        html: '../templates/index.html',
+        js: '../static/js/partials/app.min.js'
     },
     watch: {
-        html: '../templates/*.html'
+        html: '../templates/*.html',
+        js: '../static/js/partials/*.js'
     }
 };
 
 var config = {
     server: {
-        baseDir: "/"
+        baseDir: "../",
+        index: "index.htm"
     },
     tunnel: true,
     host: 'localhost',
@@ -31,8 +36,9 @@ var config = {
 };
 
 gulp.task('webserver', function () {
-    browserSync(config);
+    browserSync(config)
 });
+
 
 
 gulp.task('html:build', function () {
@@ -41,18 +47,34 @@ gulp.task('html:build', function () {
         .pipe(gulp.dest(path.build.html));
 });
 
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        //      .pipe(uglify()) //Сожмем наш js
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
+
+
+
 gulp.task('clean', function (cb) {
     rimraf(path.clean, cb);
 });
 
 gulp.task('build', [
-    'html:build'
+    'html:build',
+    'js:build'
 ]);
 
 
 gulp.task('watch', function () {
     watch([path.watch.html], function (event, cb) {
         gulp.start('html:build');
+    });
+
+    watch([path.watch.js], function (event, cb) {
+        gulp.start('js:build');
     });
 });
 
